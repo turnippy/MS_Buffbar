6923c15214deef2e4c289a2919c1282521126afb
Tong <tongdavid1liu@gmail.com>
2024-03-23 13:49:14 -0700

    added image load

--- display.py
+++ display.py
@@ -19,15 +19,26 @@ class Display:
         self.images = {}
 
     def load_images(self, imgs_in):
+        """
+        :param imgs_in: dcit with keys = buff name, value = np array of image
+        :return: None
+        """
         for item in config.BUFFS_LIST:
             self.images[item] = imgs_in[item]
 
     def update(self, imgs):
+        """
+        :param imgs: dict with keys = buff name, value = np array of image
+        :return: None
+        """
         self.load_images(imgs)
         self.root.after(100, self.update)
 
 
 if __name__ == "__main__":
     test = Display()
-    test.update()
+    tdict = {}
+    for i in config.BUFFS_LIST:
+        tdict[i] = None
+    test.update(tdict)
     test.root.mainloop()
