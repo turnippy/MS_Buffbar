c8097a87132c127bff87168f182d19fa92007193
Tong <tongdavid1liu@gmail.com>
2024-03-24 13:22:04 -0700

    updated main to use threads

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-import sys
-import os
-import mss
-import cv2
-import numpy as np
-
-import config
-import display
-import screencapture
+import threading
 
 from time import sleep
 
+import display
+import screencapture
+
+
+class App:
+    def __init__(self):
+        self.bb = screencapture.BuffBar()
+        self.d = display.Display()
+
+        self.thread_active = False
+        self.thread = threading.Thread(target=self.get_and_load_imgs, daemon=True)
+
+    def get_and_load_imgs(self):
+        while self.thread_active:
+            self.d.load_images(self.bb.buff_imgs)
+            sleep(100)
+
+    def start(self):
+        self.thread_active = True
+        self.thread.start()
+
+    def stop(self):
+        self.thread_active = False
+
 
 def main():
-    bb = screencapture.BuffBar()
-    # cv2.imshow('test', bb.ms_curr)
-    # cv2.waitKey()
-    for k in bb.buff_coords.keys():
-        if bb.buff_coords[k]:
-            cv2.imshow('test', bb.buff_imgs[k])
-            cv2.waitKey()
-    return
-
+    a = App()
+    a.bb.start()
+    a.start()
+    a.d.start()
 
 if __name__ == '__main__':
     main()
