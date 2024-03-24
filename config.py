5ebb11a06d763e33a12fefb422ed5a65c3062c58
Tong <tongdavid1liu@gmail.com>
2024-03-24 13:22:27 -0700

    fixed formatting

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ List of global config variables used for MS_Buffbar
 
 BUFFS_LIST = ["phantom_blade", "growth_spurt", "continuous_ring"]
 
-BUFF_ZOOM_DIM = 320  # 320 * 320 dimension for each buff icon in enhanced buff bar
+BUFF_ZOOM_DIM = 200  # dimension for each buff icon in enhanced buff bar
--- util.py
+++ util.py
@@ -6,13 +6,15 @@ import config
 
 
 class ImgWrapper:
-    def __init__(self, img_np, blank=False):
-        self.blank = blank
+    def __init__(self, img_np=None, blank=False):
+        self.isblank = blank
         self.img_np = img_np
         self.img_tk = None
 
     def resize_to_config(self):
-        self.img_np = np.resize(self.img_np, (config.BUFF_ZOOM_DIM, config.BUFF_ZOOM_DIM))
+        if not self.isblank:
+            self.img_np = np.resize(self.img_np, (config.BUFF_ZOOM_DIM, config.BUFF_ZOOM_DIM))
 
     def convert_to_tk(self):
-        self.img_tk = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(self.img_np))
+        if not self.isblank:
+            self.img_tk = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(self.img_np))
